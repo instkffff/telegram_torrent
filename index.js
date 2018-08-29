d74eb9ee3cad6d302ce44a2e8459b3961b73d078
NightCandle <instkffff@gmail.com>
2018-08-29 16:23:44 +0800

    .

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ bot.start((ctx) => ctx.reply('Welcome! please check /help for more information')
 bot.help((ctx) => ctx.reply('just for personal use torrent client via telegram, if you want use please contact NightCandle for permission.'))
 
 //torrent magnetic
-bot.command('magnetic',(ctx) => {
+bot.command('magnetic',({ctx,replay}) => {
 	let magnetic_url = ctx.state.command.args
 	client.add(magnetic_url,{path: './file_save'}, 
 		function(torrent){
-			console.log('torrent download start')
+			replay('torrent download start')
 			torrent.on('done',function(){
-				console.log('torrent download finished')
+				replay('torrent download finished')
 				var file_list = torrent.files.find(
 					function(file_list){
 						return file.path.endsWith(['.mp4','.m4v','.mkv','.avi'])
@@ -34,23 +34,23 @@ bot.command('magnetic',(ctx) => {
 })
 
 //current download list
-bot.command('onDownload',(ctx) => {
+bot.command('onDownload',({ctx,replay}) => {
 	torrent.on('download',function(bytes){
-		ctx.replay('just downloaded:' + bytes)
-		ctx.replay('total downloaded:' + torrent.downloaded)
-		ctx.replay('progress:' + torrent.progress )
+		replay('just downloaded:' + bytes)
+		replay('total downloaded:' + torrent.downloaded)
+		replay('progress:' + torrent.progress )
 	})
 
 })
 
 //destroy
-bot.command('destroy',(ctx) => {
+bot.command('destroy',({ctx,replay}) => {
 	let torrent_id = ctx.state.command.args
 	client.remove(torrent_id,
 		function(){
-			ctx.replay('remove torrent successful')
+			replay('remove torrent successful')
 		}).catch(function(err){
-			ctx.replay('please contact NightCandle for help')
+			replay('please contact NightCandle for help')
 		})
 })
 
