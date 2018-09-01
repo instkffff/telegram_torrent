d39924df44f2d0f7e2a68c889658d7982b64086b
NightCandle <instkffff@gmail.com>
2018-09-01 11:17:52 +0800

    .

--- test/upload.js
+++ test/upload.js
@@ -1,5 +1,9 @@
+const Telegraf = require('telegraf')
+const commandParts = require('telegraf-command-parts')
+const fileManager = require('file-manager-js')
 bot.command('uploadVideo',(ctx) => { 
 	let Viedo = ctx.state.command.args
+	console.log(ctx.state.command.args)
 	ctx.replyWithVideo({
 		source: './file_save/bbb_sunflower_1080p_60fps_stereo_abl.mp4'
 	})
