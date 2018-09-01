0505d11c5c52ae9ac1370556abeca3d3d71a3095
NightCandle <instkffff@gmail.com>
2018-09-01 10:24:36 +0800

    .

--- file.js
+++ file.js
@@ -5,13 +5,7 @@ const fileManager = require('file-manager-js');
 
 function listPath(Path){
 	fileManager.listDeep(Path)
-		.then((entries) => {
-			var Files = entries.files
-			var Dirs = entries.dirs
-			var fileList = Object.assign(Files,Dirs) 
-		})
 		.catch((error) => {'listPath failed'})
-	return fileList
 }
 
 function removeFLoder(Path){
